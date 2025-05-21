ae44d32be7c3eb8ea3adb241ce6433ba71f4bd91
Zayan Farazi <z5590941@ad.unsw.edu.au>
2025-05-21 19:28:31 +1000

    feature: using variables inside of react

--- devsoc-webpage/src/message.tsx
+++ devsoc-webpage/src/message.tsx
@@ -1,5 +1,6 @@
 function Message() {
     const name = 'Zayan';
+    // {} allows you to use any js/ts code within it, whether its a function, expression etc
     return <h1>Hello {name}!</h1>;
 }
 
