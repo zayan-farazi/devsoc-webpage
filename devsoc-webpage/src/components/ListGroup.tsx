0861e84eb01ba42477e17402fe994dd8fcc45333
Zayan Farazi <z5590941@ad.unsw.edu.au>
2025-05-21 21:42:26 +1000

    feature: dynamically traversed and printed an array

--- devsoc-webpage/src/components/ListGroup.tsx
+++ devsoc-webpage/src/components/ListGroup.tsx
@@ -1,15 +1,17 @@
 function ListGroup() {
+  const list = ["first", "second", "third"];
+
   return (
     // empty <> allows for multiple elements within the one function
     <>
       <h1>Hello World, this is my list!</h1>
-      <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
-      </ul>
+      <ul className="list-group"></ul>
+
+      {/* This is how to dynamically use an array in jsx */}
+      {list.map((item) => (
+        // keys allow for elements to be uniquely identified
+        <li key={item}> {item}</li>
+      ))}
     </>
   );
 }
