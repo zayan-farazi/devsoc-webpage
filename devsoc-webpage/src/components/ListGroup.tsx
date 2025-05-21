0aff3fc239f63be9d7c9a0137729af4b43930637
Zayan Farazi <z5590941@ad.unsw.edu.au>
2025-05-21 21:31:06 +1000

    feature: multiple elements within the one component

--- devsoc-webpage/src/components/ListGroup.tsx
+++ devsoc-webpage/src/components/ListGroup.tsx
@@ -1,12 +1,16 @@
 function ListGroup() {
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    // empty <> allows for multiple elements within the one function
+    <>
+      <h1>Hello World, this is my list!</h1>
+      <ul className="list-group">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+      </ul>
+    </>
   );
 }
 
