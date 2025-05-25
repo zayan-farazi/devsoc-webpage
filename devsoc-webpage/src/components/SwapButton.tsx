837d6115fe93f3f08bd076d0b633cafb651db782
Zayan Farazi <z5590941@ad.unsw.edu.au>
2025-05-25 16:11:29 +1000

    fix: for debugging, made sure that clicking the swap button prints "swap triggered"

--- devsoc-webpage/src/components/SwapButton.tsx
+++ devsoc-webpage/src/components/SwapButton.tsx
@@ -1,6 +1,11 @@
 function SwapButton() {
   return (
-    <button type="button" className="btn btn-primary btn-lg">
+    <button
+      type="button"
+      className="btn btn-primary btn-lg"
+      // onClick requires a function to run when clicked, in this case we used an empty lambda that prints swap triggered
+      onClick={() => console.log("Swap Triggered")}
+    >
       Click me please!
     </button>
   );
