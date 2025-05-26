0f06a403e8a95830c3449c8ef080cceeb70941b1
Zayan Farazi <zayan.farazi1@gmail.com>
2025-05-26 13:40:11 +1000

    fix; amended the layout using css class

--- devsoc-webpage/src/App.tsx
+++ devsoc-webpage/src/App.tsx
@@ -11,32 +11,34 @@ function App() {
 
   return (
     <>
-      <div className="container-fluid vh-100 d-flex flex-column p-0">
-        <div className="row g-0 flex-grow-1">
-          <div className="d-flex flex-column vh-100">
-            {/* this renders the page according to whether the page is flipped or not */}
-            {isInverted ? (
-              <>
-                <BottomHalf>
-                  <Message></Message>
-                </BottomHalf>
-                <TopHalf>
-                  <Message></Message>
-                </TopHalf>
-              </>
-            ) : (
-              <>
-                <TopHalf>
-                  <Message></Message>
-                </TopHalf>
-                <BottomHalf>
-                  <Message></Message>
-                </BottomHalf>
-              </>
-            )}
-            <SwapButton onClick={() => setIsInverted((prev) => !prev)}>
-              {isInverted ? "Unflip & Uninvert" : "Flip & Invert"}
-            </SwapButton>
+      <div className={isInverted ? "inverted" : ""}>
+        <div className="container-fluid vh-100 d-flex flex-column p-0">
+          <div className="row g-0 flex-grow-1">
+            <div className="d-flex flex-column vh-100">
+              {/* this renders the page according to whether the page is flipped or not */}
+              {isInverted ? (
+                <>
+                  <BottomHalf>
+                    <Message></Message>
+                  </BottomHalf>
+                  <TopHalf>
+                    <Message></Message>
+                  </TopHalf>
+                </>
+              ) : (
+                <>
+                  <TopHalf>
+                    <Message></Message>
+                  </TopHalf>
+                  <BottomHalf>
+                    <Message></Message>
+                  </BottomHalf>
+                </>
+              )}
+              <SwapButton onClick={() => setIsInverted((prev) => !prev)}>
+                {isInverted ? "Unflip & Uninvert" : "Flip & Invert"}
+              </SwapButton>
+            </div>
           </div>
         </div>
       </div>
