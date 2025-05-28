222e5e89ec86adcfd0ad9789c628f8c405630447
Zayan Farazi <z5590941@ad.unsw.edu.au>
2025-05-28 22:00:44 +1000

    fix: edited table entries

--- devsoc-webpage/src/components/skills-table.tsx
+++ devsoc-webpage/src/components/skills-table.tsx
@@ -1,19 +1,19 @@
 // creating arrays for tables almost souls-like meters
 const normalSkills = [
-  { skill: "React & Next.js", level: 95 },
+  { skill: "Cooking", level: 95 },
   { skill: "TypeScript", level: 90 },
-  { skill: "Node.js & APIs", level: 88 },
-  { skill: "Photography", level: 85 },
-  { skill: "UI/UX Design", level: 82 },
-  { skill: "Creative Problem Solving", level: 98 },
+  { skill: "Blowing up Capacitators", level: 88 },
+  { skill: "Cricket", level: 85 },
+  { skill: "Creative Problem Solving", level: 82 },
+  { skill: "Curating Music Playlists", level: 98 },
 ];
 
 // should only be seen when flipped button is clicked
 const flippedSkills = [
   { skill: "Procrastinating", level: 95 },
   { skill: "Dodging MyExperience Surveys", level: 88 },
-  { skill: "Perspective Shifting", level: 92 },
-  { skill: "Color Inversion", level: 85 },
-  { skill: "Scroll Reversal", level: 90 },
-  { skill: "Time Dilation", level: 78 },
+  { skill: "Watching Reels", level: 92 },
+  { skill: "Skipping Lectures", level: 85 },
+  { skill: "Grinding RPGs", level: 90 },
+  { skill: "Watching Indian men on YouTube coding", level: 78 },
 ];
