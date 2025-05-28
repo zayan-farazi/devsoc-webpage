67336f2ed9eca3be1b70d1c1f7db942216571872
Zayan Farazi <z5590941@ad.unsw.edu.au>
2025-05-29 00:10:58 +1000

    fix: created an about me header and styled it

--- devsoc-webpage/src/components/about-me.tsx
+++ devsoc-webpage/src/components/about-me.tsx
@@ -10,7 +10,19 @@ export function AboutSection() {
         filter: isFlipped ? "invert(1) hue-rotate(180deg)" : "none",
       }}
     >
-      <div className="max-w-4xl mx-auto"></div>
+      <div className="max-w-4xl mx-auto">
+        <h3
+          // this is the styling for the "About Me" header text
+          className={`text-3xl font-bold text-center mb-12 transition-colors duration-1000 ${
+            isFlipped ? "text-yellow-400" : "text-purple-400"
+          }`}
+          style={{
+            transform: isFlipped ? "scaleY(-1)" : "scaleY(1)",
+          }}
+        >
+          About Me
+        </h3>
+      </div>
     </section>
   );
 }
