98eb70b25ed2c241abb014980a04a9f4845d4a22
Zayan Farazi <z5590941@ad.unsw.edu.au>
2025-05-28 21:52:31 +1000

    feature: creating a souls-like table for "skills"

---
+++ devsoc-webpage/src/components/skills-table.tsx
@@ -0,0 +1,19 @@
+// creating arrays for tables almost souls-like meters
+const normalSkills = [
+  { skill: "React & Next.js", level: 95 },
+  { skill: "TypeScript", level: 90 },
+  { skill: "Node.js & APIs", level: 88 },
+  { skill: "Photography", level: 85 },
+  { skill: "UI/UX Design", level: 82 },
+  { skill: "Creative Problem Solving", level: 98 },
+];
+
+// should only be seen when flipped button is clicked
+const flippedSkills = [
+  { skill: "Procrastinating", level: 95 },
+  { skill: "Dodging MyExperience Surveys", level: 88 },
+  { skill: "Perspective Shifting", level: 92 },
+  { skill: "Color Inversion", level: 85 },
+  { skill: "Scroll Reversal", level: 90 },
+  { skill: "Time Dilation", level: 78 },
+];
