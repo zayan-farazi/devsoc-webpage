705cd9feb3ce1ff734d5c06ba8859b19c2297099
Zayan Farazi <z5590941@ad.unsw.edu.au>
2025-05-30 00:31:53 +1000

    more debugging

--- devsoc-webpage/vite.config.ts
+++ devsoc-webpage/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/devsoc-webpage/',   // ← add this line
   plugins: [react()],
+  // ...any other config you already have...
 })
