f04b17a71da21d372f0eadc904b6cb998499aeaf
Zayan Farazi <z5590941@ad.unsw.edu.au>
2025-05-29 19:45:58 +1000

    fix: clicking the button now turns the page 180deg

--- devsoc-webpage/src/App.tsx
+++ devsoc-webpage/src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import "./App.css";
 import AboutSection from "./components/about-section";
 import ConnectSection from "./components/connect-section";
 import ExperiencesSection from "./components/experiences-section";
@@ -7,13 +6,14 @@ import IntroSection from "./components/intro-section";
 import SkillsSection from "./components/skills-section";
 import SwapButton from "./components/SwapButton";
 import "./index.css";
+import "./App.css";
 
 export default function App() {
   const [swapped, setSwapped] = useState(false);
   const handleSwap = () => setSwapped((f) => !f);
 
   return (
-    <div className={`app-container ${swapped ? "swapped" : ""}`}>
+    <div className={`app-container ${swapped ? " swapped" : ""}`}>
       <SwapButton onFlip={handleSwap} Swapped={swapped}></SwapButton>
       <IntroSection></IntroSection>
       <AboutSection></AboutSection>
