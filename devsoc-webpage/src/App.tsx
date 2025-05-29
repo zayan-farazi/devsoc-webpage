8704c6c4302b56ff781fbe65628e56d95802f96c
Zayan Farazi <z5590941@ad.unsw.edu.au>
2025-05-29 19:08:06 +1000

    fix: cleaned up implementation by splitting into further components

--- devsoc-webpage/src/App.tsx
+++ devsoc-webpage/src/App.tsx
@@ -1,15 +1,20 @@
 import "./App.css";
 import AboutSection from "./components/about-section";
+import ConnectSection from "./components/connect-section";
+import ExperiencesSection from "./components/experiences-section";
 import IntroSection from "./components/intro-section";
 import SkillsSection from "./components/skills-section";
 import "./index.css";
 
 export default function App() {
   return (
-    <>
+    <div className="app-container">
+      <SkillsSection></SkillsSection>
       <IntroSection></IntroSection>
       <AboutSection></AboutSection>
-      <SkillsSection></SkillsSection>
-    </>
+
+      <ExperiencesSection></ExperiencesSection>
+      <ConnectSection></ConnectSection>
+    </div>
   );
 }
